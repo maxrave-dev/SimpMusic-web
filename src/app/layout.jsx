1bd7f5e5742a8e911abc6ed48b1564a7a98004e0
maxrave-dev <ndtminh2608@gmail.com>
2025-05-25 11:31:23 +0700

    ✨ feat(ads.txt): Thêm tệp ads.txt với thông tin nhà xuất bản Google Ads.

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -26,13 +26,7 @@ const RootLayout = ({ children }) => {
             )}
         >
         <Providers>
-            <Script
-                async
-                src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4666740922614578"
-                strategy="afterInteractive"
-                crossOrigin="anonymous"
-            />
-            <Header/>
+            <Header />
             {children}
             <Footer/>
             <div itemScope itemType="https://schema.org/WebSite">
